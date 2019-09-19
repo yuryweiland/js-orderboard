f322fc6a518a7c36c951bc185f7d0dcec15eaa9f
Yury Vayland <yurem@bk.ru>
2019-09-19 14:34:53 +0300

    feat(orders-board): добавлена генерация списков заказов на табло

--- js/orders-board.js
+++ js/orders-board.js
@@ -3,6 +3,12 @@ var dayOrders = [];
 var processingOrders = [];
 var readyOrders = [];
 
+var processingOrdersList = document.getElementById('processingOrders');
+var readyOrdersList = document.getElementById('readyOrders');
+
+var processingOrdersListFragment = document.createDocumentFragment();
+var readyOrdersListFragment = document.createDocumentFragment();
+
 var todayDate = new Date();
 todayDate.setMinutes(0);
 todayDate.setSeconds(0);
@@ -89,6 +95,51 @@ function deleteOrder(currentOrder) {
     setOrders();
 }
 
+/**
+ * Рендерим внешний вид записи о заказе
+ * @param order
+ * @returns {HTMLDivElement}
+ */
+function renderOrder(order) {
+    var orderRecord = document.createElement("div");
+    orderRecord.className="order";
+    orderRecord.innerHTML = '<p>' + order.orderNumber + '</p>';
+    return orderRecord;
+}
+
+/**
+ * Генерируем в html списки заказов ("готовятся" и "готовы")
+ */
+function generateOrderLists() {
+    let processingResult = [];
+    let readyResult = [];
+
+    getObjectFromLocalStorage('orders').forEach((order) => {
+
+        if (order.status === 'processing') {
+            processingResult.push(renderOrder(order));
+        } else if (order.status === 'ready') {
+            readyResult.push(renderOrder(order));
+        }
+
+    });
+
+    for(var i = 0; i < processingResult.length; i++) {
+        processingOrdersListFragment.appendChild(processingResult[i]);
+    }
+    for(var i = 0; i < readyResult.length; i++) {
+        readyOrdersListFragment.appendChild(readyResult[i]);
+    }
+
+    processingOrdersList.appendChild(processingOrdersListFragment);
+    readyOrdersList.appendChild(readyOrdersListFragment);
+}
+
+/**
+ * Возвращает объект из localStorage
+ * @param objectName
+ * @returns {any}
+ */
 function getObjectFromLocalStorage(objectName) {
     return JSON.parse(localStorage.getItem(objectName));
 }
@@ -96,14 +147,20 @@ function getObjectFromLocalStorage(objectName) {
 orders = [
     {
         id: 1,
-        status: 'processing'
+        status: 'ready',
+        orderNumber: '12'
     },
     {
         id: 2,
-        status: 'processing'
+        status: 'processing',
+        orderNumber: '13'
+    },
+    {
+        id: 2,
+        status: 'processing',
+        orderNumber: '14'
     },
 ];
 
-// setOrders();
-
-document.getElementById('processingOrders').innerHTML = getObjectFromLocalStorage('orders');
+setOrders();
+generateOrderLists();
